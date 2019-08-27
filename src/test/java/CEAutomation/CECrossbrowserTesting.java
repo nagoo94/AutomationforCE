860f759f1da8e070e7c60a24cdd66a99c464b215
Nagabushan Kashyap <Nagabushan94@gmail.com>
2019-08-27 16:25:21 +0530

    Added validation for close button

--- src/test/java/CEAutomation/CECrossbrowserTesting.java
+++ src/test/java/CEAutomation/CECrossbrowserTesting.java
@@ -2,6 +2,7 @@ package CEAutomation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -42,10 +43,18 @@ public class CECrossbrowserTesting {
     @Test
     public void Test1()
     {
+        //Entering the URL
         driver.get("https://www.linkedin.com");
-        System.out.println(driver.getTitle());
+        System.out.println(driver.getTitle()); // capturing the Title of the page
 
-        driver.findElement(By.cssSelector("body > nav > section.sign-in-card.show > button > li-icon")).click();
+        WebElement crossbutton = driver.findElement(By.cssSelector("body > nav > section.sign-in-card.show > button > li-icon"));;
+        Boolean j = crossbutton.isDisplayed();
+        if (j = true)
+        {
+
+            driver.findElement(By.cssSelector("body > nav > section.sign-in-card.show > button > li-icon")).click();
+        }
+
         driver.findElement(By.linkText("Join now")).click();
         driver.findElement(By.id("first-name")).clear();
         driver.findElement(By.id("first-name")).sendKeys("Nagabushan");
@@ -55,6 +64,11 @@ public class CECrossbrowserTesting {
         driver.findElement(By.id("join-email")).sendKeys("Nagabushan@ce.com");
         driver.findElement(By.id("join-password")).clear();
         driver.findElement(By.id("join-password")).sendKeys("qwerty@1234");
+
+        //Commenting as it will show a validation
+        //driver.findElement(By.id("submit-join-form-text")).click();
+
+        //Clicking on "Continue with Facebook"
         driver.findElement(By.cssSelector("#uno-reg-join > div > div > div > div.content-container > div.reg-content-wrapper.single > div.join-form-container.form-container > div.join-form-wrapper.form-wrapper > div.third-party-btn-container > button")).click();
 
     }
